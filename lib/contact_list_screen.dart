4508f0a6fff3b33a9fb5ab4f387ceb9ade19187d
Lyziane <lyziane_lyzi@hotmail.com>
2024-09-27 11:41:47 -0400

    Atualizações nas telas de contato e principal

--- lib/contact_list_screen.dart
+++ lib/contact_list_screen.dart
@@ -34,31 +34,47 @@ class _ContactListScreenState extends State<ContactListScreen> {
       appBar: AppBar(
         title: Text('Agenda de Contatos'),
       ),
-      body: ListView.builder(
-        itemCount: contacts.length,
-        itemBuilder: (context, index) {
-          final contact = contacts[index];
-          return ListTile(
-            title: Text(contact.name),
-            subtitle: Text('${contact.phone} - ${contact.email}'),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () => _deleteContact(index),
-            ),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ContactFormScreen(
-                    contact: contact,
-                    onSave: (updatedContact) => _editContact(updatedContact, index),
+      body: contacts.isEmpty
+          ? Center(
+              child: Text(
+                'Nenhum contato encontrado!',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: contacts.length,
+              itemBuilder: (context, index) {
+                final contact = contacts[index];
+                return ListTile(
+                  title: Text(contact.name),
+                  subtitle: Text('${contact.phone} - ${contact.email}'),
+                  trailing: Wrap(
+                    spacing: 12, // Space between buttons
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.edit),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ContactFormScreen(
+                                contact: contact,
+                                onSave: (updatedContact) =>
+                                    _editContact(updatedContact, index),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteContact(index),
+                      ),
+                    ],
                   ),
-                ),
-              );
-            },
-          );
-        },
-      ),
+                );
+              },
+            ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
